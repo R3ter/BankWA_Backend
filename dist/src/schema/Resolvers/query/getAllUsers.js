0936dab8f1f269258ac56364a8d40ae358bc53d6
waleed <waleed.sukhon77@gmail.com>
2023-03-25 07:14:03 +0300

    filter was added to frontned

--- dist/src/schema/Resolvers/query/getAllUsers.js
+++ dist/src/schema/Resolvers/query/getAllUsers.js
@@ -5,14 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const UserModel_1 = __importDefault(require("../../../../models/UserModel"));
 const graphql_fields_1 = __importDefault(require("graphql-fields"));
-exports.default = async (_, { sortBy, filter }, _context, info) => {
+exports.default = async (_, { sortBy, filter, onlyActive }, _context, info) => {
     const sort = {};
     sort[sortBy] = -1;
-    return await UserModel_1.default.find(filter
-        ? {
-            cash: { $gt: filter?.starts, $lt: filter?.ends },
-        }
-        : {})
+    return await UserModel_1.default.find({
+        $and: [
+            { ...(onlyActive ? { active: true } : {}) },
+            {
+                ...(filter?.starts != undefined && filter?.ends
+                    ? {
+                        cash: { $gt: filter?.starts, $lt: filter?.ends },
+                    }
+                    : {}),
+            },
+        ],
+    })
         .select(Object.keys((0, graphql_fields_1.default)(info)))
         .sort(sort);
 };
--- src/schema/Resolvers/query/getAllUsers.ts
+++ src/schema/Resolvers/query/getAllUsers.ts
@@ -3,19 +3,25 @@ import UserModel from "../../../../models/UserModel";
 import graphqlFields from "graphql-fields";
 export default async (
   _,
-  { sortBy, filter },
+  { sortBy, filter, onlyActive },
   _context,
   info: GraphQLResolveInfo
 ) => {
   const sort = {};
+
   sort[sortBy] = -1;
-  return await UserModel.find(
-    filter
-      ? {
-          cash: { $gt: filter?.starts, $lt: filter?.ends },
-        }
-      : {}
-  )
+  return await UserModel.find({
+    $and: [
+      { ...(onlyActive ? { active: true } : {}) },
+      {
+        ...(filter?.starts != undefined && filter?.ends
+          ? {
+              cash: { $gt: filter?.starts, $lt: filter?.ends },
+            }
+          : {}),
+      },
+    ],
+  })
     .select(Object.keys(graphqlFields(info)))
     .sort(sort);
 };
